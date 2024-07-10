83b5a71cc4680d6e35b3ea77564ada3cda0fda4b
LianTRV <samialianagustin@gmail.com>
2024-07-10 10:11:10 -0300

    Proyecto terminado

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,16 +4,6 @@ const path = require("path");
 const router = express.Router();
 const productsFilePath = path.join(__dirname, '../data/products.json');
 
-/* // Funcion para leer productos de un JSON
-const readProducts = () => {
-    const data = fs.readFileSync(productsFilePath, 'utf8');
-    return JSON.parse(data);
-}; */
-
-/* // Funcion para escribir productos de un JSON
-const writeProducts = (products) => {
-    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2), 'utf8');
-}; */
 
 // Brinda el archivo HTML
 router.get("/superAdmin/crearProducto", (req, res) => {
